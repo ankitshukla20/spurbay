74b168d5544118d116f430db7dd4cd7fdb86164c
Ankit Shukla <ankit2099shukla@gmail.com>
2024-03-20 20:02:45 +0530

    Add global catch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
+import createHttpError, { isHttpError } from "http-errors";
 
 dotenv.config();
 
@@ -8,6 +9,22 @@ const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server of Spurbayfashions" });
+});
+
+app.use((req, res, next) => {
+  next(createHttpError(404, "Endpoint not found"));
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  let statusCode = 500;
+  let errorMessage = "Internal Server Error";
+
+  if (isHttpError(err)) {
+    statusCode = err.statusCode;
+    errorMessage = err.message;
+  }
+
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 app.listen(port, () => {
