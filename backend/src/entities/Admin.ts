639161af321af646137e98c1f08f017ce8cbce08
Ankit Shukla <ankit2099shukla@gmail.com>
2024-03-23 08:40:25 +0530

    Reset password with email for admin.

--- backend/src/entities/Admin.ts
+++ backend/src/entities/Admin.ts
@@ -9,3 +9,11 @@ export interface AdminSigninBody {
   email: string;
   password: string;
 }
+export interface AdminForgotPasswordBody {
+  email: string;
+}
+
+export interface AdminResetPasswordBody {
+  password: string;
+  confirmPassword: string;
+}
--- backend/src/routes/adminAuth.ts
+++ backend/src/routes/adminAuth.ts
@@ -1,13 +1,26 @@
 import { Router } from "express";
-import { signupSchema, signinSchema } from "../models/auth";
+import {
+  signupSchema,
+  signinSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema,
+} from "../models/auth";
 import createHttpError from "http-errors";
 import { prisma } from "../utils/client";
-import { Admin, AdminSigninBody } from "../entities/Admin";
+import {
+  Admin,
+  AdminForgotPasswordBody,
+  AdminResetPasswordBody,
+  AdminSigninBody,
+} from "../entities/Admin";
 import {
   comparePasswords,
   generateHashedPassword,
+  generateResetToken,
   generateToken,
 } from "../utils/authMethods";
+import { emailTemplate } from "../utils/emailTemplate";
+import { sendEmail } from "../utils/email";
 
 const router = Router();
 
@@ -92,6 +105,104 @@ router.post("/logout", async (req, res, next) => {
     res.clearCookie("token", { httpOnly: true, secure: true });
 
     res.json({ message: "Logged Out" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* ---- Send Mail To Reset Password  ---- */
+
+router.post("/password/forgot", async (req, res, next) => {
+  try {
+    // Checks
+    const { email } = req.body as AdminForgotPasswordBody;
+    const validation = forgotPasswordSchema.safeParse({ email });
+    if (!validation.success) {
+      throw createHttpError(400, "Invalid Forgot Password Inputs");
+    }
+
+    const admin = await prisma.admin.findUnique({ where: { email } });
+
+    if (!admin) throw createHttpError(404, "Admin Not Found");
+
+    // Generate reset token
+    const resetToken = generateResetToken();
+
+    // Save reset token and expiry in the database
+    await prisma.admin.update({
+      where: { id: admin.id },
+      data: {
+        resetPwToken: resetToken,
+        resetPwTokenExpiry: new Date(Date.now() + 1000 * 60 * 30), // 30 minutes from now
+      },
+    });
+
+    // Send mail
+    const resetURL = `http://localhost:3000/api/auth/admin/password/reset/${resetToken}`;
+    const username = admin.firstname;
+
+    const message = emailTemplate(username, resetURL);
+
+    await sendEmail({
+      to: admin.email,
+      subject: "Spurbay Admin Password Recovery",
+      message,
+    });
+
+    res.json({ message: `Email sent to ${admin.email}` });
+  } catch (err) {
+    await prisma.admin.update({
+      where: { email: req.body.email as string },
+      data: {
+        resetPwToken: null,
+        resetPwTokenExpiry: null,
+      },
+    });
+
+    next(err);
+  }
+});
+
+/* ---- Reset Password  ---- */
+
+router.put("/password/reset/:token", async (req, res, next) => {
+  try {
+    // Checks for token and finding user
+    const resetToken = req.params.token;
+
+    const admin = await prisma.admin.findFirst({
+      where: { resetPwToken: resetToken },
+    });
+
+    if (
+      !admin ||
+      !admin.resetPwTokenExpiry ||
+      admin.resetPwTokenExpiry < new Date()
+    ) {
+      throw createHttpError(400, "Invalid or Expired Token");
+    }
+
+    // Checks for body
+    const validation = resetPasswordSchema.safeParse(req.body);
+    if (!validation.success)
+      throw createHttpError(400, "Invalid Reset Password Inputs");
+
+    // Reset Password
+    const { password, confirmPassword } = req.body as AdminResetPasswordBody;
+    if (password !== confirmPassword)
+      throw createHttpError(400, "Password Doesn't Match");
+
+    const hashedPassword = await generateHashedPassword(password);
+    await prisma.admin.update({
+      where: { id: admin.id },
+      data: {
+        password: hashedPassword,
+        resetPwToken: null,
+        resetPwTokenExpiry: null,
+      },
+    });
+
+    res.json({ message: "Password Reset Successful" });
   } catch (err) {
     next(err);
   }
